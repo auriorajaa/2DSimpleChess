d9d70109a0108028e3c1f18314a1f2beeb54f12e
Aurio Rajaa <riorajaa2018@gmail.com>
2024-03-01 16:08:35 +0700

    Adding Icon

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,16 @@
 package main;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Main {
+    static ImageIcon logo = new ImageIcon(Main.class.getClassLoader().getResource("res/chess.png"));
+
     public static void main(String[] args) {
         JFrame window = new JFrame("Simple Chess");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
+        window.setIconImage(logo.getImage());
 
         // Add GamePanel to the window
         GamePanel gp = new GamePanel();
